36b471520c2ed91fcb19392b53926550da3dda4f
amuehlbauer <andreas.muehlbauer@ianus-it.de>
2017-02-11 13:26:09 +0100

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+cp -R /defaults /etc/dovecot
+sed -i "s/SERVICENAME/${SERVICENAME}/g" /etc/dovecot/dovecot.conf
+sed -i "s/SERVICENAME/${SERVICENAME}/g" /etc/dovecot/conf.d/10-ssl.conf
+sed -i "s/EMAIL/${EMAIL}/g" /etc/dovecot/conf.d/20-lmtp.conf
 rsyslogd
 /usr/sbin/dovecot -c /etc/dovecot/dovecot.conf
 tail -F /var/log/dovecot.log
